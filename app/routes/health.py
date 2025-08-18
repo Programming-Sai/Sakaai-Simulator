224333b77a511d163a703e009091036d25a16501
Programming-Sai <saiahprog6@gmail.com>
2025-08-18 15:14:26 +0000

    Trying to fix config data not showing

--- app/routes/health.py
+++ app/routes/health.py
@@ -3,33 +3,7 @@ import os
 from fastapi import APIRouter
 from app.models.schema import HealthResponse
 from langchain_core.prompts import PromptTemplate
-import json
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-max_file_size = int(os.getenv("MAX_FILE_SIZE_MB", "3"))
-max_requests_per_day = int(os.getenv("MAX_REQUEST_PER_DAILY", "5/day").split("/")[0])
-max_number_of_questions_per_generation = int(os.getenv("MAX_NUM_QUESTIONS", "30"))
-
-feedback_questions_raw = os.getenv("FEEDBACK_QUESTIONS", "[]")
-try:
-    feedback_questions = json.loads(feedback_questions_raw)
-except Exception as e:
-    print(str(e))
-    feedback_questions = []
-
-
-
-
-config = {
-    "max_file_size":max_file_size,
-    "max_number_of_questions_per_generation":max_number_of_questions_per_generation,
-    "max_requests_per_day":max_requests_per_day, 
-    "feedback_questions":feedback_questions
-}
-
+from app.utils.config import get_config
 
 router = APIRouter()
 
@@ -59,7 +33,7 @@ async def health_check():
     healthy = all(results.values())
     status  = "healthy" if healthy else "unhealthy"
     message = "Sakaai Simulator is up and running." if healthy else "One or more components failed health check."
-    response = {"status": status, "message": message, "config":config}
+    response = {"status": status, "message": message, "config":get_config()}
     if not healthy:
         response["failures"] = {k: v for k, v in results.items() if not v}
     # print("Config: ", response)
---
+++ app/utils/config.py
@@ -0,0 +1,29 @@
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_config():
+
+    max_file_size = int(os.getenv("MAX_FILE_SIZE_MB", "3"))
+    max_requests_per_day = int(os.getenv("MAX_REQUEST_PER_DAILY", "5/day").split("/")[0])
+    max_number_of_questions_per_generation = int(os.getenv("MAX_NUM_QUESTIONS", "30"))
+
+    feedback_questions_raw = os.getenv("FEEDBACK_QUESTIONS", "[]")
+    try:
+        feedback_questions = json.loads(feedback_questions_raw)
+    except Exception as e:
+        print(str(e))
+        feedback_questions = []
+
+
+
+
+    return {
+        "max_file_size":max_file_size,
+        "max_number_of_questions_per_generation":max_number_of_questions_per_generation,
+        "max_requests_per_day":max_requests_per_day, 
+        "feedback_questions":feedback_questions
+    }
