637deb029bbde89bc7947ebbb8c8fe1cad51bba6
Programming-Sai <saiahprog6@gmail.com>
2025-08-18 15:26:45 +0000

    Hopefully Fixed

--- app/utils/config.py
+++ app/utils/config.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 def get_config():
 
-    max_file_size = int(os.getenv("MAX_FILE_SIZE_MB", "3"))
+    max_file_size = int(os.getenv("MAX_FILE_SIZE_MB"))
     max_requests_per_day = int(os.getenv("MAX_REQUEST_PER_DAILY", "5/day").split("/")[0])
-    max_number_of_questions_per_generation = int(os.getenv("MAX_NUM_QUESTIONS", "30"))
+    max_number_of_questions_per_generation = int(os.getenv("MAX_NUM_QUESTIONS"))
 
     feedback_questions_raw = os.getenv("FEEDBACK_QUESTIONS", "[]")
     try:
